fe83185850c278918cc0a64dde812dbdff2f26ad
DubiousTunic <43465053+DubiousTunic@users.noreply.github.com>
2018-09-21 12:31:08 -0400

    Add files via upload

---
+++ godhand-tables/html-table-to-graph/scripts/converter.js
@@ -0,0 +1,13 @@
+var table = $(".data-table");
+
+function getTableData(table) {
+    var data = [];
+    table.find('tr').each(function (rowIndex, r) {
+        var cols = [];
+        $(this).find('th,td').each(function (colIndex, c) {
+            cols.push(c.textContent);
+        });
+        data.push(cols);
+    });
+    return data;
+}
